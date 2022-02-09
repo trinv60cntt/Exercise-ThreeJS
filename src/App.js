564fd3fd5eb4e97b368bc28f81f6dc9f08d22ed4
Nguyễn Văn Trí <91021009+trinv60cntt@users.noreply.github.com>
2022-02-09 22:49:16 +0700

    note use comment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, render, events } from '@react-three/fiber'
+
+window.addEventListener('resize', () =>
+  render(<mesh />, document.querySelector('canvas'), {
+    events,
+    size: { width: window.innerWidth, height: window.innerHeight },
+  }),
+)
+
+window.dispatchEvent(new Event('resize'))
 
 function Box() {
   const meshRef = React.useRef();
-
+  // move object
   useFrame((state, delta) => {
     delta += 0.001
     meshRef.current.rotation.x += delta
@@ -12,17 +21,21 @@ function Box() {
 
   return (
     <mesh ref={meshRef} rotation={[0, 0, 0]}>
-      <boxGeometry width={1} height={1} depth={1} />
-      <meshBasicMaterial color={0x44aa88} />
+      {/* Geometry */}
+      <boxGeometry width={1} height={1} depth={1} /> 
+      {/* Material */}
+      <meshPhongMaterial color={0x44aa88} />
     </mesh>
   )
 }
 
 function App() {
   return (
-    <Canvas camera={{ fov: 75, aspect: 2, near: 1, far: 5 }}>
+    // init Canvas - camera
+    <Canvas camera={{ fov: 75, aspect: 2, near: 1, far: 5000 }}>
       <Box />
-      <directionalLight color={0xFFFFFF} intensity={1} position={-1, 2, 4} />
+      {/* add light */}
+      <directionalLight color={0xFFFFFF} intensity={1} position={[-1, 2, 4]} />
     </Canvas>
   );
 }
